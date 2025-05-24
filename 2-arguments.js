0f53c04e5132286f0c71de92b9868fefd253d370
ShoSho011 <shawnnkeneza@gmail.com>
2025-05-24 20:28:21 +0200

    commit# Changes to be committed: commit#	new file:   2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,15 @@
+const args = process.argv;
+
+const numberOfArguments = args.length - 2;
+
+
+if (numberOfArguments === 0) {
+    
+    console.log("No argument");
+  } else if (numberOfArguments === 1) {
+
+    console.log("Argument found");
+  } else {
+   
+    console.log("Arguments found");
+  }
